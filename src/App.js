8b3d1ecfcc9efb41386f6860a5921739b718571b
unknown <contact.abhinavanshul@gmail.com>
2021-08-02 22:07:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Loader from "./Components/Loader";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+import * as ROUTES from "./constants/routes";
+
+// components lazily loaded
+const Dashboard = lazy(() => import("./Components/Dashboard/Dashboard"));
+const Login = lazy(() => import("./Components/Login/Login"));
+const Signup = lazy(() => import("./Components/Signup/Signup"));
+const ForgotPassword = lazy(() =>
+  import("./Components/ForgotPassword/ForgotPassword")
+);
+const ResetPassword = lazy(() =>
+  import("./Components/ResetPassword/ResetPassword")
+);
+const PostJob = lazy(() => import("./Components/Dashboard/PostJob"));
+const AppliedJobs = lazy(() => import("./Components/Dashboard/AppliedJobs"));
+const PrivateRoute = lazy(() => import("./Components/Util/PrivateRoute"));
+const Header = lazy(() => import("./Components/Header/Header"));
+const Home = lazy(() => import("./Components/Home/Home"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Header />
+        <Switch>
+          <Route path={ROUTES.HOME} exact component={Home}></Route>
+          <Route path={ROUTES.LOGIN} component={Login}></Route>
+          <Route path={ROUTES.SIGNUP} component={Signup}></Route>
+          <Route
+            path={ROUTES.FORGOTPASSWORD}
+            component={ForgotPassword}
+          ></Route>
+          <Route path={ROUTES.RESETPASSWORD} component={ResetPassword}></Route>
+          <PrivateRoute path={ROUTES.DASHBOARD} component={Dashboard} />
+          <PrivateRoute
+            path={ROUTES.POSTJOB}
+            component={PostJob}
+            allowedRole={0}
+          />
+          <PrivateRoute
+            path={ROUTES.APPLIEDJOBS}
+            component={AppliedJobs}
+            allowedRole={1}
+          />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
